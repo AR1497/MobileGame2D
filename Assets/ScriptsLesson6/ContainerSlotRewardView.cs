7c413d77d106cba99a97a03d63d6a23087997478
AR1497 <vladimirova.a0501200@mail.ru>
2022-05-01 12:56:07 +0300

    Lesson6

---
+++ Assets/ScriptsLesson6/ContainerSlotRewardView.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContainerSlotRewardView : MonoBehaviour
+{
+    [SerializeField]
+    private Image _selectBackground;
+    [SerializeField]
+    private Image _iconCurrency;
+    [SerializeField]
+    private TMP_Text _textDays;
+    [SerializeField]
+    private TMP_Text _countReward;
+    public void SetData(Reward reward, int countDay, bool isSelect)
+    {
+        _iconCurrency.sprite = reward.IconCurrency;
+        _textDays.text = $"Day {countDay}";
+        _countReward.text = reward.CountCurrency.ToString();
+        _selectBackground.gameObject.SetActive(isSelect);
+    }
+}
